edea8d1806b86d78dcb39b3be6a0c63b2885fde2
R34P3RGR1M <g.slavov@yahoo.com>
2022-01-31 18:13:00 +0200

    Added some console interactions

--- src/main/java/QAAutomation/qa/QA_Java_FirstSteps.java
+++ src/main/java/QAAutomation/qa/QA_Java_FirstSteps.java
@@ -1,5 +1,24 @@
 package QAAutomation.qa;
 
+import java.util.Scanner;
+
 public class QA_Java_FirstSteps {
-    
+
+    public static void main(String[] args) {
+        System.out.println("Please enter your username");
+
+        Scanner scanner = new Scanner(System.in);
+
+        String userName = scanner.next();
+
+        System.out.println("Your username is: " + userName);
+
+        System.out.println("Please enter your favourite hobby");
+
+        String userHobby = scanner.next();
+
+        System.out.println("Your favourite hobby is: " + userHobby);
+
+
+    }
 }
